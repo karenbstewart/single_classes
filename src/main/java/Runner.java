b391dc92e2da55013b93ef47c1576fc39b0b9ad3
Karen Stewart <kastewart2002@googlemail.com>
2021-11-15 14:37:31 +0000

    first commit

---
+++ src/main/java/Runner.java
@@ -0,0 +1,9 @@
+public class Runner {
+    public static void main(String[] args) {
+        Bear bear = new Bear("Balu", 25, 95.62);
+        bear.setName("Baloo");
+        String name = bear.getName();
+        System.out.println(name);
+    }
+
+}
