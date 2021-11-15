78dc22fb992b6607b9b9ac69785e1dca05c79504
Karen Stewart <kastewart2002@googlemail.com>
2021-11-15 16:00:04 +0000

    Primative types vs Reference types

--- src/test/java/BearTest.java
+++ src/test/java/BearTest.java
@@ -36,6 +36,31 @@ public class BearTest {
     public void notReadyToHibernateOver80(){
         Bear thinBear = new Bear("Bubu", 9, 25.02);
         assertEquals(false, thinBear.readyToHibernate());
+
+        Bear bear1 = new Bear(" yogi", 65, 140 );
+        Bear bear2 = bear1;
+
+        bear2.setName("barney");
+
+        int age1 = 33;
+        int age2 = age1;
+        age1 = 50;
+
+        String str1 = "first";
+        String str2 = str1;
+        str2 = "second";
+
+        System.out.println(bear1);
+        System.out.println(bear2);
+        System.out.println(bear1.getName());
+        System.out.println(bear2.getName());
+
+        System.out.println(age1);
+        System.out.println(age2);
+
+        System.out.println(str1);
+        System.out.println(str2);
+
     }
 
 
